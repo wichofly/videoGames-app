cd57f2549234caa4d568830307a48f84a55e9a0f
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-12 20:56:48 +0100

    feat: responsive ideas created.
    focused in Chakra example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem } from '@chakra-ui/react'
 
 function App() {
 
 
   return (
-    <Button colorScheme='teal' variant='ghost' size='lg'>
-      Button
-    </Button>
+    <Grid templateAreas={`"nav nav" 
+                          "aside main"
+                          "aside footer"`}
+      gridTemplateRows={'50px 1fr 30px'}
+      gridTemplateColumns={'150px 1fr '}
+      h='200px' gap='1' color='blackAlpha.700'>
+      <GridItem pl='2' bg='orange.300' area={'nav'}>
+        Nav
+      </GridItem>
+      <GridItem pl='2' bg='blue.300' area={'aside'}>
+        Aside
+      </GridItem>
+      <GridItem pl='2' bg='green.300' area={'main'}>
+        Main
+      </GridItem>
+      <GridItem pl='2' bg='pink.300' area={'footer'}>
+        Footer
+      </GridItem>
+    </Grid >
   )
 
 }
