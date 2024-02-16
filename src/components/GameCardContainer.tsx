9d5bca85355f8e9207abcf7a239796b65e3ce535
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-16 23:27:18 +0100

    feat: make space in game cards

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,7 @@ import { ReactNode } from "react"
 
 const GameCardContainer = ({ children }: { children: ReactNode }) => {
   return (
-    <Box w='300px' borderRadius={10} overflow='hidden'>
+    <Box borderRadius={10} overflow='hidden'>
       {children}
     </Box>
   )
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = () => {
       <Text>{error && <p>{error}</p>}</Text>
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
+        spacing={3}
         padding='10px'
       >
         {isLoading && skeletons.map(skeleton =>
