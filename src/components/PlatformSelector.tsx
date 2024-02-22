52bed0bc6da69058374ff0dfc7f3ae2ca586e76b
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-22 11:05:40 +0100

    feat: build platform selector

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -1,19 +1,31 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { FaChevronDown } from 'react-icons/fa'
+import usePlatform from '../hooks/usePlatform'
 
 const PlatformSelector = () => {
+  const { data, error } = usePlatform()
+
+  if (error) return null
+
   return (
-    <Menu >
-      <MenuButton as={Button} rightIcon={<FaChevronDown />}>
-        Platform
-      </MenuButton>
-      <MenuList>
-        <MenuItem>Item 1</MenuItem>
-        <MenuItem>Item 2</MenuItem>
-        <MenuItem>Item 3</MenuItem>
-      </MenuList>
-    </Menu>
+    <>
+      {/* {error ? null : ''} */}
+
+      <Menu>
+        <MenuButton as={Button} rightIcon={<FaChevronDown />}>
+          Platform
+        </MenuButton>
+        <MenuList>
+          {data.map(platform => <MenuItem key={platform.id}>{platform.name}</MenuItem>)}
+        </MenuList>
+      </Menu >
+    </>
   )
 }
 
 export default PlatformSelector
+
+/**
+ * Using "if (error) return null"
+   It doesn't show the platform button, it is better than "{error ? null : ''}"
+ */
