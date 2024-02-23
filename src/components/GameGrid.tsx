c44e0dcf2bceb0e3b6e5589cf1b0acbb844ed586
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-23 20:06:02 +0100

    refactor: Game grid error
    Advise: There is not  need to put  SimpleGrid and the error Text next to each other and then wrap them inside a fragment.
    It is better to write it out of the return. CODE WILL BE CLEANER!

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,25 +14,24 @@ const GameGrid = ({ gameQuery }: Props) => {
 
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8]
 
+  if (error) return <Text>{error}</Text>
+
   return (
-    <>
-      <Text>{error && <p>{error}</p>}</Text>
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={4}
-        padding='10px'
-      >
-        {isLoading && skeletons.map(skeleton =>
-          <GameCardContainer key={skeleton}>
-            <GameCardSkeleton />
-          </GameCardContainer>)}
-        {data.map(game =>
-          <GameCardContainer key={game.id} >
-            <GameCard game={game} />
-          </GameCardContainer>
-        )}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={4}
+      padding='10px'
+    >
+      {isLoading && skeletons.map(skeleton =>
+        <GameCardContainer key={skeleton}>
+          <GameCardSkeleton />
+        </GameCardContainer>)}
+      {data.map(game =>
+        <GameCardContainer key={game.id} >
+          <GameCard game={game} />
+        </GameCardContainer>
+      )}
+    </SimpleGrid>
   )
 }
 
