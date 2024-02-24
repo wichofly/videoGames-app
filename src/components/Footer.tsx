c97c0d9c9697088d7fa97ec38321928d0ce13e35
Mauricio Erazo <antonierazo13@hotmail.com>
2024-02-24 16:13:52 +0100

    refactor: make the links from footer as a  ghost buttons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Link, Text } from "@chakra-ui/react"
+import { Box, Button, HStack, Link, Text } from "@chakra-ui/react"
 import { FaLinkedin, FaGithub, FaLaptop } from "react-icons/fa"
 
 interface Props {
@@ -9,18 +9,24 @@ const Footer = ({ text }: Props) => {
   return (
     <HStack p='20px' justifyContent='space-between' fontSize='xl' >
       <Text as='b' >{text}</Text>
-      <Box >
+      <Box textAlign='center'>
         <Text as='b'>Find me on</Text>
-        <HStack justifyContent='space-between' my={2} fontSize='1.3em'>
-          <Link href="https://www.linkedin.com/in/mauricio-erazo-/" isExternal>
-            <FaLinkedin />
-          </Link>
-          <Link href="https://github.com/wichofly/videoGames-app" isExternal>
-            <FaGithub />
-          </Link>
-          <Link href="https://wichofly.github.io/portfolio-website/" isExternal>
-            <FaLaptop />
-          </Link>
+        <HStack justifyContent='space-between' my={2}>
+          <Button variant='ghost'>
+            <Link href="https://www.linkedin.com/in/mauricio-erazo-/" isExternal>
+              <FaLinkedin fontSize='1.8em' />
+            </Link>
+          </Button>
+          <Button variant='ghost'>
+            <Link href="https://github.com/wichofly/videoGames-app" isExternal>
+              <FaGithub fontSize='1.8em' />
+            </Link>
+          </Button>
+          <Button variant='ghost'>
+            <Link href="https://wichofly.github.io/portfolio-website/" isExternal>
+              <FaLaptop fontSize='1.8em' />
+            </Link>
+          </Button>
         </HStack>
       </Box>
     </HStack>
