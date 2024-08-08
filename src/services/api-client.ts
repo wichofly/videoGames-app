cbfc084d6e0d4a9bfee2f7173446673c469944a8
Mauricio Erazo <antonierazo13@hotmail.com>
2024-08-08 10:11:45 +0200

    refactor: api_key remains secure

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
+const api_key = import.meta.env.VITE_API_KEY;
+
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '35f38480039a4843bcc86b3eeb3a4e4f',
+    key: api_key,
   },
 });
 
