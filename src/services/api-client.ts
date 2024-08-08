e301e307b72ec580288bc530049b0b17130263e3
Mauricio Erazo <antonierazo13@hotmail.com>
2024-08-08 11:31:34 +0200

    fix: kept showing CORS error. it is maintained like before

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,9 @@
 import axios from 'axios';
 
-const api_key = import.meta.env.VITE_API_KEY;
-
-if (!api_key) {
-  throw new Error('VITE_API_KEY is not defined in the environment variables.');
-}
-
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: api_key,
+    key: '35f38480039a4843bcc86b3eeb3a4e4f',
   },
 });
 
