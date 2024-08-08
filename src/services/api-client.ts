88db16339b303c0d98ce82b42bfd5fb67ff7619f
Mauricio Erazo <antonierazo13@hotmail.com>
2024-08-08 11:09:53 +0200

    it is showing me CORS problems. Trying to solve it

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,15 +1,13 @@
 import axios from 'axios';
 
-const api_key = import.meta.env.VITE_API_KEY;
+// const api_key = import.meta.env.VITE_API_KEY;
 
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: api_key,
+    key: '35f38480039a4843bcc86b3eeb3a4e4f',
   },
 });
-
-
 
 /**
  * The "key" ---> will be included in the query string every HTTP request is sent to the backend
