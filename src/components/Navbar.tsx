5eeee47384d754f5bd0c668f9cc5d8995f077c69
Mauricio Erazo <antonierazo13@hotmail.com>
2024-08-27 16:36:58 +0200

    style: new logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,24 @@
-import { HStack, Image } from "@chakra-ui/react"
-import logo from '../assets/logo.webp'
+import { HStack, Image } from '@chakra-ui/react';
+import logo from '../assets/logo-videoGames.webp';
 
-import ColorModeButton from "./ColorModeButton"
-import SearchInput from "./SearchInput"
+import ColorModeButton from './ColorModeButton';
+import SearchInput from './SearchInput';
 
 interface Props {
-  onSearch: (searchText: string) => void
+  onSearch: (searchText: string) => void;
 }
 
 const Navbar = ({ onSearch }: Props) => {
   return (
-    <HStack padding='10px'>
-      <Image src={logo} boxSize='60px' />
+    <HStack padding="10px">
+      <Image src={logo} boxSize="60px" borderRadius="lg" />
       <SearchInput onSearch={onSearch} />
       <ColorModeButton />
     </HStack>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
 
 /**
  * VStack: used to stack elements in the vertical direction
